f3ff6c6c2c74be5734eb5e5cd6799b6b4b2ae871
madiha-eman <madihaema1@gmail.com>
2021-02-02 13:49:24 +0500

    some code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+function Hi() {
+  return <div>
+    <center><div className="main">
+      <button><b> <a href="https://github.com/hasnainulhassan000">𝔾𝕚𝕥𝕙𝕦𝕓 𝔸𝕔𝕔𝕠𝕦𝕟𝕥</a></b></button>
+  <h1> 𝐻𝐸𝐿𝐿❀ 𝐻𝒜𝒮𝒩𝒜𝐼𝒩 </h1>
+    <hr></hr>
+    <h1>ＩＮＴＲＯＤＵＣＴＩＯＮ</h1>
+    <hr></hr>
+    <div className="intro">
+      <h3>Ｎａｍｅ :</h3><span>Hasnain ul Hasan</span>
+      <hr></hr>
+      <h3>Ｗｏｒｋ :</h3><span>Develaper</span>
+      <hr></hr>
+      <h3>Ｓｔｕｄｙ :</h3><span>Dars -e- Nizami</span>
+      <hr></hr>
+    </div>
+    <h1>Ｄｉｓｉｎｇ   ｂｙ  </h1>
+    <hr></hr>
+    <marquee><h2> Hasnain ul Hassan </h2></marquee>
+    <hr></hr>
+    <h3 className="col">ᗯ𝒽𝓐𝓉Ť𝓈Ãⓟ𝔭:   ０３４５５７５６６１８</h3> 
+    </div></center>
+      </div>
+}
+
+ReactDOM.render(<Hi/>, document.querySelector('#root'));
